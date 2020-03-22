fc6066db998657af4e6e362210fe20eb06531814
blossom31s <44717935+blossom31s@users.noreply.github.com>
2020-03-22 22:30:45 +0900

    Create AccountDTO.java

---
+++ account/AccountDTO.java
@@ -0,0 +1,35 @@
+package account;
+
+public class AccountDTO {
+
+	public String busiNum;
+	public String factory;
+	public String tradeBank;
+	public String accountNum;
+
+	public String getBusiNum() {
+		return busiNum;
+	}
+	public void setBusiNum(String busiNum) {
+		this.busiNum = busiNum;
+	}
+	public String getFactory() {
+		return factory;
+	}
+	public void setFactory(String factory) {
+		this.factory = factory;
+	}
+	public String getTradeBank() {
+		return tradeBank;
+	}
+	public void setTradeBank(String tradeBank) {
+		this.tradeBank = tradeBank;
+	}
+	public String getAccountNum() {
+		return accountNum;
+	}
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+
+}
