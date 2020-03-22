8d6ee74b04aa952b953388d150cfc08c6860180a
blossom31s <44717935+blossom31s@users.noreply.github.com>
2020-03-22 22:35:43 +0900

    Create CustomDTO.java

---
+++ custom/CustomDTO.java
@@ -0,0 +1,189 @@
+package custom;
+
+public class CustomDTO {
+
+	private String busiNum;
+	private String custom;
+	private String shortName;
+	private String ceo;
+	private String chargePerson;
+	private String busiCondition;
+	private String item;
+	private String postNum;
+	private String addr1;
+	private String addr2;
+	private String tel;
+	private String fax;
+	private String homepage;
+	private String coYN;
+	private String foreignYN;
+	private String taxYN;
+	private String countryEng;
+	private String countryKor;
+	private String specialRelation;
+	private String tradeStop;
+	private String contractPeriodS;
+	private String contractPeriodE;
+	private String regiInfoMan;
+	private String regiInfoDate;
+	private String modiInfoMan;
+	private String modiInfoDate;
+
+	public String getBusiNum() {
+		return busiNum;
+	}
+	public void setBusiNum(String busiNum) {
+		this.busiNum = busiNum;
+	}
+	public String getCustom() {
+		return custom;
+	}
+	public void setCustom(String custom) {
+		this.custom = custom;
+	}
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+	public String getCeo() {
+		return ceo;
+	}
+	public void setCeo(String ceo) {
+		this.ceo = ceo;
+	}
+	public String getChargePerson() {
+		return chargePerson;
+	}
+	public void setChargePerson(String chargePerson) {
+		this.chargePerson = chargePerson;
+	}
+	public String getBusiCondition() {
+		return busiCondition;
+	}
+	public void setBusiCondition(String busiCondition) {
+		this.busiCondition = busiCondition;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public String getPostNum() {
+		return postNum;
+	}
+	public void setPostNum(String postNum) {
+		this.postNum = postNum;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getHomepage() {
+		return homepage;
+	}
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+	public String getCoYN() {
+		return coYN;
+	}
+	public void setCoYN(String coYN) {
+		this.coYN = coYN;
+	}
+	public String getForeignYN() {
+		return foreignYN;
+	}
+	public void setForeignYN(String foreignYN) {
+		this.foreignYN = foreignYN;
+	}
+	public String getTaxYN() {
+		return taxYN;
+	}
+	public void setTaxYN(String taxYN) {
+		this.taxYN = taxYN;
+	}
+	public String getCountryEng() {
+		return countryEng;
+	}
+	public void setCountryEng(String countryEng) {
+		this.countryEng = countryEng;
+	}
+	public String getCountryKor() {
+		return countryKor;
+	}
+	public void setCountryKor(String countryKor) {
+		this.countryKor = countryKor;
+	}
+	public String getSpecialRelation() {
+		return specialRelation;
+	}
+	public void setSpecialRelation(String specialRelation) {
+		this.specialRelation = specialRelation;
+	}
+	public String getTradeStop() {
+		return tradeStop;
+	}
+	public void setTradeStop(String tradeStop) {
+		this.tradeStop = tradeStop;
+	}
+	public String getContractPeriodS() {
+		return contractPeriodS;
+	}
+	public void setContractPeriodS(String contractPeriodS) {
+		this.contractPeriodS = contractPeriodS;
+	}
+	public String getContractPeriodE() {
+		return contractPeriodE;
+	}
+	public void setContractPeriodE(String contractPeriodE) {
+		this.contractPeriodE = contractPeriodE;
+	}
+	public String getRegiInfoMan() {
+		return regiInfoMan;
+	}
+	public void setRegiInfoMan(String regiInfoMan) {
+		this.regiInfoMan = regiInfoMan;
+	}
+	public String getRegiInfoDate() {
+		return regiInfoDate;
+	}
+	public void setRegiInfoDate(String regiInfoDate) {
+		this.regiInfoDate = regiInfoDate;
+	}
+	public String getModiInfoMan() {
+		return modiInfoMan;
+	}
+	public void setModiInfoMan(String modiInfoMan) {
+		this.modiInfoMan = modiInfoMan;
+	}
+	public String getModiInfoDate() {
+		return modiInfoDate;
+	}
+	public void setModiInfoDate(String modiInfoDate) {
+		this.modiInfoDate = modiInfoDate;
+	}
+
+}
